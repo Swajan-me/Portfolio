16b59b87365112543f30bb24e3ff801199bd2455
Swajan-me <swajanshrestha3180@gmail.com>
2026-08-31 09:00:50 +0545

    Update: Kind of a refactor, addition of font.

--- js/components.js
+++ js/components.js
@@ -6,8 +6,8 @@ const navbar = `
         </a>
         <ul>
             <li><a href="/index.html">Home</a></li>
-            <li><a href="/index.html#about">About</a></li>
             <li><a href="/index.html#projects">Projects</a></li>
+            <li><a href="/index.html#about">About</a></li>
             <li><a href="/index.html#contact">Contact</a></li>
         </ul>
     </nav>
