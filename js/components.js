1c8348389228f15c1232daabe31272ccae09cf13
Swajan-me <swajanshrestha3180@gmail.com>
2026-09-03 14:57:34 +0545

    refactor: Added footer to contact page, removed/ commented the footer from components.

--- js/components.js
+++ js/components.js
@@ -13,20 +13,20 @@ const navbar = `
     </nav>
 </div>`;
 
-const footer = `
-<footer id="contact">
-    <p>Contact me: 
-        <a href="https://mail.google.com/mail/?view=cm&to=swajanshrestha3180@gmail.com" target="_blank" rel="noopener noreferrer">
-            <i class="fa-regular fa-envelope"></i>
-        </a>
-        or 
-        <a href="https://www.linkedin.com/in/swajan-rajbanshi-203955302/" target="_blank" rel="noopener noreferrer">
-            <i class="fa-brands fa-linkedin-in"></i>
-        </a>
-    </p>
-    <p>© 2026 Swajan Rajbanshi.</p>
-</footer>`;
+// const footer = `
+// <footer id="contact">
+//     <p>Contact me: 
+//         <a href="https://mail.google.com/mail/?view=cm&to=swajanshrestha3180@gmail.com" target="_blank" rel="noopener noreferrer">
+//             <i class="fa-regular fa-envelope"></i>
+//         </a>
+//         or 
+//         <a href="https://www.linkedin.com/in/swajan-rajbanshi-203955302/" target="_blank" rel="noopener noreferrer">
+//             <i class="fa-brands fa-linkedin-in"></i>
+//         </a>
+//     </p>
+//     <p>© 2026 Swajan Rajbanshi.</p>
+// </footer>`;
 
 document.body.insertAdjacentHTML('afterbegin', navbar);
 
-document.body.insertAdjacentHTML('beforeend', footer);
+// document.body.insertAdjacentHTML('beforeend', footer);
