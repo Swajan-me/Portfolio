d3cd3b277077f5dca87669552cc226a2247179c9
Swajan-me <swajanshrestha3180@gmail.com>
2026-09-07 13:27:44 +0545

    update: Fixed links for subpages, and home icon.

--- js/components.js
+++ js/components.js
@@ -2,13 +2,13 @@ const navbar = `
 <div class="container">
     <nav class="navBar">
         <a href="/index.html">
-            <img class="logo" src="/Images/Me.jpg" alt="Profile" height="50px" width="50px">
+            <img class="logo" src="./Images/dog.jpg" alt="Profile" height="50px" width="50px">
         </a>
         <ul>
-            <li><a href="/index.html">Home</a></li>
-            <li><a href="/index.html#projects">Projects</a></li>
-            <li><a href="/index.html#about">Skills</a></li>
-            <li><a href="/index.html#contact-section">Contact</a></li>
+            <li><a href="./index.html">Home</a></li>
+            <li><a href="./index.html#projects">Projects</a></li>
+            <li><a href="./index.html#about">Skills</a></li>
+            <li><a href="./index.html#contact-section">Contact</a></li>
         </ul>
     </nav>
 </div>`;
